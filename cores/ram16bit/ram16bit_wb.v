6ff408c16bf6ad6942121036bdd9358fd81cb73d
Anthony Green <green@moxielogic.com>
2016-11-20 09:30:44 -0500

    use vendor memory

--- cores/ram16bit/ram16bit_wb.v
+++ cores/ram16bit/ram16bit_wb.v
@@ -1,3 +1,5 @@
+`include "config.v"
+
 module ram16bit_wb (input         rst_i,
 		    input 	  clk_i,
 		    input [31:0]  wb_adr_i,
@@ -27,44 +29,19 @@ module ram16bit_wb (input         rst_i,
 		 .douta (wb_dat_o));
 `else
 
-  // 16k RAM
-  reg  [7:0] ram[0:16383];
-  wire [11:0] index;
+ 
+   lpm_ram_dq ram (.data(wb_dat_i), 
+		   .address(wb_adr_i[12:1]), 
+		   .we(!(wb_stb_i & wb_cyc_i & wb_we_i)),
+		   .inclock(clk_i), 
+                   .q(wb_dat_o));
 
-  assign index = wb_adr_i[11:0];
+// passing the parameter values
+   defparam ram.lpm_width = 16;
+   defparam ram.lpm_widthad = 8;
+   defparam ram.lpm_indata = "REGISTERED";
+   defparam ram.lpm_outdata = "REGISTERED";
 
-  reg [0:0]  wb_ack_o_reg;
-  reg [15:0] wb_dat_o_reg;
-
-   assign wb_ack_o = wb_ack_o_reg;
-   assign wb_dat_o = wb_dat_o_reg;
-   
-  always @(posedge clk_i)
-    begin
-      if (wb_sel_i[0])
-	wb_dat_o_reg[15:8] <= ram[index];
-      if (wb_sel_i[1])
-	wb_dat_o_reg[7:0] <= ram[index+1];
-      wb_ack_o_reg <= wb_stb_i & wb_cyc_i;
-    end
-  
-
-   always @(posedge clk_i)
-     begin
-	if (wb_stb_i & wb_cyc_i & wb_we_i)
-	  begin
-	     if (wb_sel_i[1])
-	       ram[index+1] <= wb_dat_i[7:0];
-	     if (wb_sel_i[0])
-	       ram[index] <= wb_dat_i[15:8];
-	  end
-     end
-   
-/*  ram4k16bit ram(.clock (clk_i),
-		 .wren (!(wb_stb_i & wb_cyc_i & wb_we_i)),
-		 .address (wb_adr_i[12:1]),
-		 .data (wb_dat_i),
-		 .q (wb_dat_o)); */
 `endif
 
 endmodule // ram16bit_wb
