84a92aa11868a6bb6231764322ac2329b3aec8b1
Anthony Green <green@moxielogic.com>
2017-10-28 19:35:58 -0400

    Add iverilog to test container.  Build muskoka

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,7 +3,16 @@
 # Put the moxie build tools on the path...
 export PATH=/opt/moxielogic/bin:$PATH
 
+echo "************************************************************************"
+echo Running icache testsuite
+echo "************************************************************************"
 cd /root/moxie-cores/bench
 make
 sbcl --load test.lisp
+
+echo "************************************************************************"
+echo Building moxie core with iverilog
+echo "************************************************************************"
+cd /root/muskoka/iverilog
+make
 
