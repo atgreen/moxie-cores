43af7de059a6f4987ec42d31eabae201a659a7a4
Anthony Green <green@moxielogic.com>
2017-10-30 20:41:40 -0400

    pre-load some lisp modules

--- scripts/install.lisp
+++ scripts/install.lisp
@@ -2,5 +2,10 @@
 
 (quicklisp-quickstart:install :path "/root/quicklisp/")
 
+;;; Load some things we will probably need
+(ql:quickload :FiveAM)
+(ql:quickload :cffi)
+(ql:quickload :elf)
+
 (with-open-file (out "/root/.sbclrc" :direction :output)
   (format out "(load \"/root/quicklisp\/setup.lisp\")"))
