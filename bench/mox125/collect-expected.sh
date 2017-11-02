3c16c8c6c475a3909d862c21cb308d1eefbbc350
Anthony Green <green@moxielogic.com>
2017-11-02 07:28:24 -0400

    reference bins, not src

--- bench/mox125/collect-expected.sh
+++ bench/mox125/collect-expected.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "(" 
-grep "^#" asm/*.S | sed -e "s/:# /\" . \"/g" | sed -e "s/^asm/(\"asm/g" | sed -e "s/$/\")/g"
+grep "^#" asm/*.S | sed -e "s/S:# /x\" . \"/g" | sed -e "s/^asm/(\"bin/g" | sed -e "s/$/\")/g"
 echo ")"
 
